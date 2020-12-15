fa9da2fb718a66cf07ed63d9bdb3b9c4ecc0de35
shamsiddin7 <shamsiddin2005@gmail.com>
2020-12-15 14:49:31 +0500

    side bar

--- pic-maker.js
+++ pic-maker.js
@@ -184,6 +184,14 @@ const picMake = async (weather, userPic, userName) => {
     element.textContent = tempReplace(weather.daily[0].temp.max)
   }
 
+
+  let hourlyTempSum = 0
+  for (const hour in weather.hourly) {
+    hourlyTempSum += hour.temp
+  }
+  console.log(hourlyTempSum)
+  console.log(hourlyTempSum / weather.hourly.length)
+
   const picBack = fs.readFileSync(`./pic/${weather.current.weather[0].icon}.png`, `binary`);
   const backImageBuffer = Buffer.from(picBack, `binary`);
   const backPic = getElements(pic, "back_pic");
