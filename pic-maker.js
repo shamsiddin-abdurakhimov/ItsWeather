826584b9d1fb73bab1c55de6ff18efc3c6846311
shamsiddin7 <shamsiddin2005@gmail.com>
2020-12-17 21:52:04 +0500

    test

--- pic-maker.js
+++ pic-maker.js
@@ -228,11 +228,9 @@ const picMake = async (weather, userPic, userName) => {
     element.setAttribute(`points`, graphPointsFill)
   }
   for (const hourClass of Object.keys(hoursClass)) {
-    console.log(hourClass, hoursClass[hourClass])
     for (const element of getElements(pic, hourClass)) {
       var date = new Date(2011, 0, 1, 0, 0, 0, 0);
       date.setSeconds(weather.hourly[hoursClass[hourClass]].dt)
-      console.log(date.toLocaleString("en-US", {hour: 'numeric'}), weather.hourly[hoursClass[hourClass]].dt)
       element.textContent = date.toLocaleString("en-US", {hour: 'numeric'})
     }
   }
