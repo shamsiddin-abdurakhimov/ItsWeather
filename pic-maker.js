d2e8e5a8442613a8d33674e19195c6cfa1e921f8
shamsiddin7 <shamsiddin2005@gmail.com>
2020-12-20 01:22:01 +0500

    test

--- pic-maker.js
+++ pic-maker.js
@@ -159,7 +159,6 @@ const picMake = async (weather, userPic, userName) => {
     let today = new Date()
     today.setSeconds(today.getUTCSeconds() + weather.timezone_offset)
     const hh = tempReplace(today.getUTCHours())
-    console.log(hh, today.getHours())
     const mm = tempReplace(today.getUTCMinutes())
     const day = weekDays[today.getUTCDay()]
     const data = today.getUTCDate()
@@ -226,7 +225,8 @@ const picMake = async (weather, userPic, userName) => {
         if (i == 0) {
           element.textContent = 'now'
         } else {
-          let date = new Date(weather.hourly[i].dt * 1000);
+          let date = new Date(today.getUTCMilliseconds() + weather.hourly[i].dt * 1000);
+          console.log(data)
           element.textContent = date.getHours();
           //element.setAttribute(`x`, x)
         }
