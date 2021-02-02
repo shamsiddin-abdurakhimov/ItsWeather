e62e73cebfe1728b7a97b043c58aeecf6ae0990e
shamsiddin7 <shamsiddin2005@gmail.com>
2021-02-02 22:15:37 +0500

    fix

--- bot.js
+++ bot.js
@@ -87,7 +87,7 @@ const sendRes = async (context) => {
   }
   console.log(2);
   console.log(
-    await client.query(`SELECT now WHERE "user_id=$1 FROM "users"`, [
+    await client.query(`SELECT now WHERE user_id=$1 FROM "users"`, [
       update.message.from.id,
     ])
   );
